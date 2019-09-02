295729eafccd9a38b9901096e006059e8b21c1f0
Dennis Vriend <dennis.vriend@booking.com>
2019-09-02 10:50:38 +0000

    filter registration config

--- 16-simple-web-filter/src/main/java/com/github/dnvriend/config/FilterRegistrationConfig.java
+++ 16-simple-web-filter/src/main/java/com/github/dnvriend/config/FilterRegistrationConfig.java
@@ -1,49 +1,40 @@
 package com.github.dnvriend.config;
 
-import com.github.dnvriend.filters.FirstFilter;
-import com.github.dnvriend.filters.SecondFilter;
-import com.github.dnvriend.filters.ThirdFilter;
-import java.util.Arrays;
 import java.util.List;
 import javax.servlet.Filter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
-/**
- * Does not work, because @Bean FilterRegistrationBean and @Bean Filters are picked up via
- * classpath scanning, indexed and initialized and added to the ServletContextInitializerBean
- */
 @Slf4j
 @Configuration
 @ConditionalOnProperty(prefix = "com.dnvriend.filter", value = "config", havingValue = "true")
-@EnableConfigurationProperties(FilterProperties.class)
-public class FilterRegistrationConfig implements BeanFactoryPostProcessor  {
+public class FilterRegistrationConfig {
 
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
-        throws BeansException {
-        List<Class <? extends Filter>> filters = Arrays.asList(
-            FirstFilter.class,
-            ThirdFilter.class,
-            SecondFilter.class
-        );
-        initializeFilters(filters, (DefaultListableBeanFactory) beanFactory);
+    @Bean
+    public static BeanFactoryPostProcessor beanFactoryPostProcessor(final Environment environment) {
+        return beanFactory -> {
+            BindResult<FilterProperties> bindResult = Binder.get(environment)
+                .bind("com.dnvriend.filter", FilterProperties.class);
+            FilterProperties filterProperties = bindResult.get();
+            if(beanFactory instanceof DefaultListableBeanFactory) {
+                DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+                initializeFilters(filterProperties.getFilters(), defaultListableBeanFactory);
+            }
+        };
     }
 
-    public FilterRegistrationConfig() {
-        log.debug("Created FilterRegistrationConfig via default constructor");
-    }
-
-    public void initializeFilters(List<Class<? extends Filter>> filters, DefaultListableBeanFactory beanFactory) {
+    private static void initializeFilters(List<Class<? extends Filter>> filters, DefaultListableBeanFactory beanFactory) {
         filters.forEach(filterClass -> {
             String filterBeanName = determineBeanName(filterClass);
             log.debug("Register filter: {} with name: {}", filterClass.getName(), filterBeanName);
@@ -72,7 +63,8 @@ public class FilterRegistrationConfig implements BeanFactoryPostProcessor  {
     private static String determineFilterRegistrationName(Class<? extends Filter> filterClass) {
         String registrationBeanName = "FilterRegistrationBean";
         String filterBeanName = filterClass.getSimpleName();
-        return String.format("%s%s", registrationBeanName, filterBeanName);
+        String filterRegistrationName = String.format("%s%s", registrationBeanName, filterBeanName);
+        return filterRegistrationName;
     }
 
     private static GenericBeanDefinition createFilterBeanDefinition(
